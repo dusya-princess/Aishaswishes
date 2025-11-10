fb3ef85fd2448ce307fb061f13b41ead188da3d3
dusya-princess <salmaprincess1210@gmail.com>
2025-11-10 18:01:28 +0200

    creating snowflakes

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ function nextwish(){
     document.getElementsByClassName("wish")[0].innerHTML=list[currentwish-1];
     document.getElementsByClassName('imgWish')[0].src=`wishes pics/${imagelist[currentwish-1]}`;
 }
+
+function createsnowflake(){
+    var snowflake=document.createElement('div');
+    snowflake.innerHTML='❄';
+
+    snowflake.classList.add('flake');
+    snowflake.style.left=Math.random()*100+'vw';
+    var size=Math.random();
+    snowflake.style.fontSize=size*20+7+'px';
+    snowflake.style.animationDuration=14-(size*10)+'s';
+    document.body.appendChild(snowflake);
+    setTimeout(()=>
+        {
+            snowflake.remove();
+        }
+        , 8000);
+
+}
+
+setInterval(createsnowflake,200);
